959a4bccb8fe15481c25476bb5357bcd177a84cd
volodymyrb <volodymyrb@fornova.net>
2020-09-17 00:44:06 +0300

    register

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request: HttpRequest):
@@ -37,3 +37,22 @@ def dashboard(request: HttpRequest):
         'section': 'dashboard'
     }
     return render(request, 'account/dashboard.html', context)
+
+
+def register(request: HttpRequest):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user: User = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            context = {
+                'new_user': new_user
+            }
+            return render(request, 'account/register_done.html', context)
+    else:
+        user_form = UserRegistrationForm()
+    context = {
+        'user_form': user_form,
+    }
+    return render(request, 'account/register.html', context)
