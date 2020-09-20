fcb799b7403ebd470d7e24607211129085e87a2b
volodymyrb <volodymyrb@fornova.net>
2020-09-20 14:19:21 +0300

    like system

--- images/views.py
+++ images/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpRequest
 from django.contrib import messages
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -37,4 +38,24 @@ def image_detail(request: HttpRequest, image_id: int, slug: str):
         'image': image,
     }
     return render(request, 'images/image/detail.html', context)
+
+
+@login_required
+@require_POST
+def image_like(request: HttpRequest):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
 
