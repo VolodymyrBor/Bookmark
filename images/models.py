348383050e8f1e9e61783f1dbaea42cd694b1395
volodymyrb <volodymyrb@fornova.net>
2020-09-28 00:11:12 +0300

    optimized likes

--- images/models.py
+++ images/models.py
@@ -11,6 +11,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
 
     # foreign keys
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='image_created', on_delete=models.CASCADE)
